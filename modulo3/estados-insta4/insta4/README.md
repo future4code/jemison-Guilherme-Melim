c0f9fd190ad161f8d3a77b3c724fd80102981ff0
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-07-05 22:17:55 -0300

    exercicio completo

--- modulo3/estados-insta4/insta4/README.md
+++ modulo3/estados-insta4/insta4/README.md
@@ -1,2 +1,2 @@
 # link surge
-unruly-volcano.surge.sh
+oval-loss.surge.sh
--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -9,26 +9,30 @@ const MainContainer = styled.div`
   align-items: center;
 `
 function App() {
+
+  const arraryPoste = [
+    {nomeUsuario:'paulinha', fotoUsuario:'https://picsum.photos/50/50', fotoPost:'https://picsum.photos/200/150'},
+    { nomeUsuario:'vanessa', fotoUsuario:'https://picsum.photos/40/40', fotoPost:'https://picsum.photos/199/150'},
+    {nomeUsuario:'guilherme', fotoUsuario:'https://picsum.photos/45/45', fotoPost:'https://picsum.photos/198/150'}
+  ]
+
+  const mapeado = arraryPoste.map((item, index) => {
+    return (
+      <Post key={index}
+           nomeUsuario = {item.nomeUsuario}
+           fotoUsuario = {item.fotoUsuario}
+           fotoPost = {item.fotoPost}
+      />
+    )
+  })
+
+  console.log(mapeado)
+
 return(
+
   <MainContainer>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-
-          <Post
-            nomeUsuario={'vanessa'}
-            fotoUsuario={'https://picsum.photos/40/40'}
-            fotoPost={'https://picsum.photos/199/150'}
-          />
-
-          <Post
-            nomeUsuario={'guilherme'}
-            fotoUsuario={'https://picsum.photos/45/45'}
-            fotoPost={'https://picsum.photos/198/150'}
-          />
-        </MainContainer>
+       {mapeado}
+    </MainContainer>
 )
 
 }
