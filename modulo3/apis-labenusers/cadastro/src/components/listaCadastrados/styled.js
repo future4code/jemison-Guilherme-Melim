7543ac4d64777641f57298cde28462357b3da061
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-07-28 23:38:43 -0300

    Observar comentário no feedback

---
+++ modulo3/apis-labenusers/cadastro/src/components/listaCadastrados/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const DisplayLista = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 10vw auto;
+    padding: 1vw;
+    border: 2px solid;
+    border-radius: 10px;
+    width: 30%;
+`
+
+export const ItemUsuario = styled.li`
+display: flex;
+justify-content: space-between;
+margin: 0.3vw
+`
