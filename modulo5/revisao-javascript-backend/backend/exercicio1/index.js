574cfe70a65f5554068abdb0452c8af48022b519
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-10-05 17:58:25 -0300

    Exercicio 1 completo

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,1 +1,20 @@
 console.log("exercicio 1");
+
+// N °C + 273,15 = Resposta K - formula de graus para Kelvin
+// (N °C × 9 /5) + 32 = Resposta °F
+
+let escolha = 'K'   //'Digite F ou K para selecionar'
+let numeroC = 25      //'Digite um numero'
+
+const soma = () =>{
+
+    if(escolha === 'K'){
+    const respostaK = numeroC + 273.15
+    return respostaK
+    }else if(escolha === 'F'){
+    const respostaF = numeroC * 9 / 5 + 32
+    return respostaF
+    }
+    
+}
+ console.log(soma(numeroC))
