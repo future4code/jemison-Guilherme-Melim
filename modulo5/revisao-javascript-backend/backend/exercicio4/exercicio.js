5ee332fec95f74c8fd2c2ef71ba2232f199c4e6a
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-10-07 18:16:17 -0300

    Exercicio 4 completo

--- modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,26 @@
 console.log("exercicio 4");
+
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	}
+]
+
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	}
+]
+const juntando = [...primeiraLista, ...segundaLista]
+
+let juntos = juntando.filter((a) => {
+	return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
+}, Object.create(null))
+
+console.log(juntos)
