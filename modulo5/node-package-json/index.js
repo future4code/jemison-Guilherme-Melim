dd44c9dc092d619a1f9067846d94e1f4fcbaf24c
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-10-11 11:11:29 -0300

    Exercicio 3 completo

--- modulo5/node-package-json/index.js
+++ modulo5/node-package-json/index.js
@@ -1,15 +1,15 @@
 // Para acessar os parâmetros: process.argv[]
-
+/*
 const nome = 'Guilherme'
 const idade = Number(25)
 const seteAnos = Number(7)
 const soma = idade + seteAnos
 
-// console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`)
-
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`)
+*\
 
 // ==================================================================== Exercicio 2
-
+/*
 const sinal = process.argv[2]
 const numbUm = Number(process.argv[3])
 const numbDois = Number(process.argv[4])
@@ -29,8 +29,21 @@ switch(sinal){
       return numbUm - numbDois
      break
     default:
-       return 'Escola uma operação existente.'
+      return 'Escola uma operação existente.'
     break
 }
 }
 console.log(opreracao())
+*/
+// ============================================================================== Exercicio 3
+
+const adicional = process.argv[2]
+const tarefas= [
+    'Lavar a louça',
+    'Varrer a casa',
+    'Passar pano'
+]
+
+ tarefas.push(adicional)
+
+console.table(tarefas)
