c529c01c5870487c46a535407ee892ccbd10011f
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-10-11 10:29:06 -0300

    Exercicio 2 completo

--- modulo5/node-package-json/index.js
+++ modulo5/node-package-json/index.js
@@ -5,4 +5,32 @@ const idade = Number(25)
 const seteAnos = Number(7)
 const soma = idade + seteAnos
 
-console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`)
+// console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`)
+
+
+// ==================================================================== Exercicio 2
+
+const sinal = process.argv[2]
+const numbUm = Number(process.argv[3])
+const numbDois = Number(process.argv[4])
+
+const opreracao = () =>{
+switch(sinal){
+    case 'soma':
+      return numbUm + numbDois
+    break
+    case 'mult':
+      return numbUm * numbDois
+    break
+    case 'div':
+      return numbUm / numbDois
+    break
+    case 'menos':
+      return numbUm - numbDois
+     break
+    default:
+       return 'Escola uma operação existente.'
+    break
+}
+}
+console.log(opreracao())
