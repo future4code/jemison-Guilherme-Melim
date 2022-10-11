dfc9428931d1c9b78f9db828d7aa1ad264e8b7da
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-10-11 12:04:32 -0300

    Exercicio 1 completo

---
+++ modulo5/node-package-json/Exercicios/exercicio1/index.js
@@ -0,0 +1,8 @@
+// Para acessar os parâmetros: process.argv[]
+
+const nome = 'Guilherme'
+const idade = Number(25)
+const seteAnos = Number(7)
+const soma = idade + seteAnos
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}`)
