ece1124527db8704c2095882f373e4eed8166dd1
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-10-17 19:52:36 -0300

    Exercicio 3

--- modulo5/intro-typescript/src/exercicio3.ts
+++ modulo5/intro-typescript/src/exercicio3.ts
@@ -1,12 +1,9 @@
-const digitado = process.argv[2]
+const digitado = Number(process.argv[2])
 
-function checaAnoBissexto(ano:any):boolean {
+function checaAnoBissexto(ano:number):boolean {
     const cond1 = ano % 400 === 0
     const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
     return cond1 || cond2
 }
 
 console.log(checaAnoBissexto(digitado))
-
-/*Usei o ANY pois foi o unico jeito que encontrei para fazer o "process.argv", mas caso for
-errado, e só excluir a linha 1 e digitar um numero dentro da function na linha 9!...*/
