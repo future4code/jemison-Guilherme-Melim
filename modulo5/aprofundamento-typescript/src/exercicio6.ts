59b145307d9c4c945b638a47e08672ef6bf3d55d
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-10-18 23:33:51 -0300

    Exercicio 6

--- modulo5/aprofundamento-typescript/src/exercicio6.ts
+++ modulo5/aprofundamento-typescript/src/exercicio6.ts
@@ -14,32 +14,22 @@ const roupasEValores: Roupas[] =[
 function filtrandoDesconto(roupasEValores:Roupas[], vestimenta:string) {
     return roupasEValores.filter(
         (item) => {
-        return item.classificacao == vestimenta    
+        return (item.classificacao == vestimenta)
+
+    }).map((item):string =>{
+        if(vestimenta === 'verao'){
+            return (`Valor da ${vestimenta} desconto é de R$${ item.preco * 0.95}`)
+
+       }else if(vestimenta === 'inverno'){
+            return (`Valor da ${vestimenta} desconto é de R$${item.preco * 0.90}`)
+
+       }else if(vestimenta === 'banho'){
+            return (`Valor da ${vestimenta} desconto é de R$${item.preco * 0.96}`)
+
+       }else if(vestimenta === 'intima'){
+            return (`Valor da ${vestimenta} desconto é de R$${item.preco * 0.93}`)
+       }       
     })
 }
 
 console.log(filtrandoDesconto(roupasEValores, process.argv[2]))
-
-const filtrando = roupasEValores.map((item)=>{
-    if(item.classificacao === 'verao' && process.argv[2]){
-         (`Valor com desconto R$${ item.preco * 0.95}`)
-    }else if
-        (item.classificacao === 'inverno' && process.argv[2]){
-         (`Valor com desconto R$${item.preco * 0.90}`)
-    }else if
-        (item.classificacao === 'banho' && process.argv[2]){
-         (`Valor com desconto R$${item.preco * 0.96}`)
-    }else if
-        (item.classificacao === 'intima' && process.argv[2]){
-         (`Valor com desconto R$${item.preco * 0.93}`)
-    }
-    return 
-}
-)
-
-console.log(filtrando)
-
-// Cada uma tem a sua própria porcentagem de desconto: 5% (verão), 10% (inverno), 4% (banho) e 7% (íntimas). 
-
-// a) Escreva uma função que receba um array de produtos com nome, preço e classificação;
-//  e retorne um array com essas informações e um campo mais: "preço com desconto"
