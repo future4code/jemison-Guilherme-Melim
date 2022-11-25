80142c1d67f1d6d004f10a7299a9f2af6a67a32e
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-11-25 13:38:04 -0300

    Arrumando

--- modulo5/servicos-backend/src/index.ts
+++ modulo5/servicos-backend/src/index.ts
@@ -1,3 +1,4 @@
+import { connection } from './connection';
 import express, {Request, Response} from "express"
 import cors from 'cors'
 
@@ -6,6 +7,51 @@ app.use(express.json())
 app.use(cors())
 
 
+app.get("/buscando/:type/:page", async(req: Request,res: Response): Promise<void> =>{
+
+    try {
+ 
+       let sort = req.query.sort
+       let order = req.query.order
+       let name = req.query.name 
+       let type = req.params.type
+       let page = Number(req.params.page)
+
+       let offSet = 5 * (page - 1)
+       
+      const result = await connection.raw(`
+         SELECT id, name, email, type
+         FROM aula48_exercicio WHERE
+         name = "${name}" AND type = "${type}"
+         ORDER BY ${sort} ${order}
+         LIMIT 5 OFFSET ${offSet};
+      `)
+       
+      //  if(!page){
+      //     page = 1
+      //    }
+
+
+      //  console.log(offSet)
+      //  if(!name && !type){
+      //    name = ""
+      //  }
+ 
+      //  if(!sort){
+      //     sort = "name"
+      //  }
+ 
+      //  if(!order){
+      //     order = "DESC"
+      //  }
+        
+       res.status(200).send(result[0])
+       
+    } catch (error:any) {
+       console.log(error)
+       res.send(error.message || error.sqlMessage)
+    }
+ })
  
  app.listen(3003, () => {
      console.log("Server is running in http://localhost:3003");
