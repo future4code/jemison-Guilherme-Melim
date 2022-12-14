96fbd3ce5d9ddb7fab76c6666da901a2647f2e62
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-12-14 12:04:43 -0300

    Exercicios 1 e 2 ok

---
+++ modulo6/heranca-encapsulamento/src/endpoints/createProduct.ts
@@ -0,0 +1,40 @@
+import { UserDatabase } from './../database/UserDatabase';
+import { Request, Response } from "express"
+import { BaseDataBase } from "../database/BaseDatabase"
+import connection from "../database/connection"
+import { TABLE_PRODUCTS } from "../database/tableNames"
+import { Product } from "../models/Product"
+
+export const createProduct = async (req: Request, res: Response) => {
+    let errorCode = 400
+    try {
+        const name = req.body.name
+        const price = req.body.price
+
+        if (!name || !price) {
+            throw new Error("Body inválido.")
+        }
+
+        // const newProduct: Product = {
+        //     id: Date.now().toString(),
+        //     name,
+        //     price
+        // }
+
+        const product = new Product(
+            Date.now().toString(),
+            name,
+            price
+        )
+
+        await connection(TABLE_PRODUCTS).insert({
+            id: product.getId(),
+            name: product.getName(),
+            price: product.getPrice()
+        })
+        
+        res.status(201).send({ message: "Produto criado", product: product })
+    } catch (error) {
+        res.status(errorCode).send({ message: error.message })
+    }
+}
