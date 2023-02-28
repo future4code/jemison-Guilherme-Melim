7eef3a8d5cb79a2570768c270a9e54b484eeae29
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2023-02-28 17:37:04 -0300

    Ajuste

--- modulo7/testes-automatizados/tests/userSaldo.test.ts
+++ modulo7/testes-automatizados/tests/userSaldo.test.ts
@@ -1,28 +1,20 @@
 import { performPurchase } from "../src/function/funcUserSaldo"
 
 describe('Teste para função do extrato do usuario', () =>{
+    const user1 = {
+        name: "guilherme",
+        balance: 1200
+    }
     test("test maior valor da compra", ()=>{
-        const user1 = {
-            name: "guilherme",
-            balance: 1200
-        }
         return expect(performPurchase(user1, 200)).toEqual({"balance": 1000, "name": "guilherme"})
     })
 
     test("test igual valor da compra", ()=>{
-        const user2= {
-            name: "guilherme",
-            balance: 1200
-        }
-        return expect(performPurchase(user2, 1200)).toEqual({"balance": 0, "name": "guilherme"})
+        return expect(performPurchase(user1, 1200)).toEqual({"balance": 0, "name": "guilherme"})
     })
 
     test("test menor valor da compra", ()=>{
-        const user3 = {
-            name: "guilherme",
-            balance: 1200
-        }
-        return expect(performPurchase(user3, 1400)).not.toEqual({ balance: 0})
+        return expect(performPurchase(user1, 1400)).not.toEqual({ balance: 0})
     })
     
 })
